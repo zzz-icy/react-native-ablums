7cc6878bd262a151f29af2bf9391de22a29f09e2
Han Zhang <eveline1129@163.com>
2018-11-08 22:18:14 -0600

    added Header

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 /** @format */
+// In the latest version of React Native you must import React from 'react' package
 
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Header from './src/components/Header';
 import { name as appName } from './app.json';
 
+const App = () => (
+    <Header />
+)
+
+// register at least one component
 AppRegistry.registerComponent(appName, () => App);
