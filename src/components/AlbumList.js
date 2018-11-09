913b6386efd4d1bba75f3aceeb801c2acb3f9843
Han Zhang <eveline1129@163.com>
2018-11-09 15:59:12 -0600

    added Album detail and card

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends React.Component {
-    componentWillMount() {
-        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(
-                (response) => (console.log(response.data))
-            );
+
+    state = {
+        albums: []
     }
+
+    componentDidMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            // .then(response => console.log(response.data));
+            .then(
+                (response) => (this.setState({
+                    albums: response.data
+                }))
+            )
+    }
+    renderAlbums() {
+        this.state.albums.map((album) => (
+            <AlbumDetail key={album.title} albumInfo={item} />
+        ))
+    }
+
     // we need some logics to handle when http requests completes, rerender the component to show th data
     render() {
+        // const { albums } = this.state;
+        // if (albums === []) return 'nodata';
+        console.log(albums);
         return (
             <View>
-                <Text>AlbumList</Text>
+                {this.renderAlbums()}
             </View>
         )
     }
---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Card = () => {
+    const { containerStyle } = styles;
+    return (
+        <View style={containerStyle}></View>
+    );
+}
+
+const styles = {
+    containerStyle: {
+
+    }
+}
+
+export default Card;
